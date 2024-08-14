ebdc5c4f90cea6eac5fce79e24856e2c3a64dfc6
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-08-14 23:24:18 +0700

    qualifiers

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/nhung/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,12 @@
+package com.nhung.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice for Baseball Coach";
+    }
+}
---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/nhung/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,12 @@
+package com.nhung.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice for Tennis Coach";
+    }
+}
