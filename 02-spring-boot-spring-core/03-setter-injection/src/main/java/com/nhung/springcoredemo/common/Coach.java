60804389c486c6e809c89db967da97d43cb8fa88
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-08-14 23:16:01 +0700

    setter injection

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/nhung/springcoredemo/common/Coach.java
@@ -0,0 +1,6 @@
+package com.nhung.springcoredemo.common;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/nhung/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.nhung.springcoredemo.rest;
+
+import com.nhung.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for dependency
+    private Coach newCoach;
+
+    // setter injection
+    @Autowired
+    public void setCoach(Coach theCoach) {
+        newCoach = theCoach;
+    }
+
+    @GetMapping("/dailyWorkout")
+    public String dailyWorkout() {
+        return newCoach.getDailyWorkout();
+    }
+}
