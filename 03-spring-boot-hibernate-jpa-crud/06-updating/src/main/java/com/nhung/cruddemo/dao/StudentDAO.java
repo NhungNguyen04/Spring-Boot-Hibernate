76f542e1ea89ac08428744ee9b6c2ed36c3df5b9
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-08-27 14:40:20 +0700

    updating objects

---
+++ 03-spring-boot-hibernate-jpa-crud/06-updating/src/main/java/com/nhung/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,18 @@
+package com.nhung.cruddemo.dao;
+
+import com.nhung.cruddemo.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+
+    void save(Student student);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String lastName);
+
+    void update(Student student);
+}
