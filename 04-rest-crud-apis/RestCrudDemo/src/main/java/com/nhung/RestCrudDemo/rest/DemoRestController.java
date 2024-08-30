7b1e54c311522fd030334b5aada0efcef8d51e9c
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-08-30 12:34:00 +0700

    exception handling

--- 04-rest-crud-apis/RestCrudDemo/src/main/java/com/nhung/RestCrudDemo/rest/DemoRestController.java
+++ 04-rest-crud-apis/RestCrudDemo/src/main/java/com/nhung/RestCrudDemo/rest/DemoRestController.java
@@ -2,10 +2,11 @@ package com.nhung.RestCrudDemo.rest;
 
 import com.nhung.RestCrudDemo.DAO.StudentDAOImpl;
 import com.nhung.RestCrudDemo.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.nhung.RestCrudDemo.errorresponse.StudentErrorResponse;
+import com.nhung.RestCrudDemo.exception.StudentNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,6 +34,22 @@ public class DemoRestController {
     @GetMapping("/students/{studentId}")
     public Student getStudentById(@PathVariable int studentId) {
         List<Student> students = studentDAO.findAll();
+
+        if(studentId > students.size() || studentId < 0) {
+            throw new StudentNotFoundException("Student not found for id " + studentId);
+        }
         return students.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exception) {
+
+        StudentErrorResponse error = new StudentErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                exception.getMessage(),
+                System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
