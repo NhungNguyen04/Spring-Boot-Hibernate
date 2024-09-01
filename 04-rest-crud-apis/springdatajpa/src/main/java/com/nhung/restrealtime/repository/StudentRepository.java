6314700942ddc06483c6738b5f164c48204a7bbf
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-09-01 14:34:37 +0700

    spring rest configuration, pagintion, sorting

--- 04-rest-crud-apis/springdatajpa/src/main/java/com/nhung/restrealtime/repository/StudentRepository.java
+++ 04-rest-crud-apis/springdatajpa/src/main/java/com/nhung/restrealtime/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package com.nhung.restrealtime.repository;
 
 import com.nhung.restrealtime.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="class12a1")
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 }
