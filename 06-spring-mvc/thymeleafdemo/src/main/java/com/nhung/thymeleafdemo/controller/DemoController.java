34c23807f414cab5fe682a85180a217d6bc1fb35
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-09-05 19:27:30 +0700

    show and process form

--- 06-spring-mvc/thymeleafdemo/src/main/java/com/nhung/thymeleafdemo/controller/DemoController.java
+++ 06-spring-mvc/thymeleafdemo/src/main/java/com/nhung/thymeleafdemo/controller/DemoController.java
@@ -4,14 +4,25 @@ package com.nhung.thymeleafdemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DemoController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String home(Model theModel) {
         theModel.addAttribute("theDate", java.time.LocalDate.now());
         return "helloworld";
     }
 
+    @RequestMapping("/showform")
+    public String showForm() {
+        return "showform";
+    }
+
+    @RequestMapping("/submit-form")
+    public String submitForm() {
+        return "submitform";
+    }
 }
